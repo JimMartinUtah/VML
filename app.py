ddc59060723732b2092ec7777a63c99ed9151ff9
JimMartinUtah <jim.martin@utah.edu>
2026-09-02 09:00:24 -0600

    excursion

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ ui.div(
 with ui.sidebar(open="desktop", width="320px"):
             ui.input_slider("onset", "Onset (% of cycle)", min=0, max=30, value=22),
             ui.input_slider("offset", "Offset (% of cycle)", min=25, max=75, value=66),
-            ui.input_slider("excursion", "Excursion amplitude (mm)", min=1, max=50, value=20),
+            ui.input_slider("excursion", "Excursion amplitude (mm)", min=1, max=40, value=20),
             ui.input_slider("cycle_freq", "Cycle frequency (Hz)", min=0.5, max=5.0, value=2.0, step=0.5),
             ui.input_numeric("length_optimal", "Length optimal (m)", value=0.084),
             ui.input_numeric("max_isometric_force", "Max isometric force (N)", value=1871),
